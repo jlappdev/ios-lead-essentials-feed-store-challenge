66dbae3a8d5a88b1382da98dad1b2f9240d97400
jlappdev <jlappdev>
2021-02-12 14:59:56 -0500

    Add Core Data Managed Object Model and Model Objects.

---
+++ ManagedCache+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ManagedCache+CoreDataClass.swift
+//  Tests
+//
+//  Created by JL Dev on 2/12/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedCache)
+public class ManagedCache: NSManagedObject {
+
+}
---
+++ ManagedFeedImage+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ManagedFeedImage+CoreDataClass.swift
+//  Tests
+//
+//  Created by JL Dev on 2/12/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ManagedFeedImage)
+public class ManagedFeedImage: NSManagedObject {
+
+}
